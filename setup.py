0a3702a4c0032c296f58e93a1bf441cb28d4248c
Gavin Cho <gavin.k.cho@gmail.com>
2024-03-19 16:59:31 -0400

    Fix client_id validation

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name="soundcloud-v2",
-    version="1.3.1",
+    version="1.3.2",
     description="Python wrapper for the v2 SoundCloud API",
     long_description=readme(),
     long_description_content_type='text/markdown',
