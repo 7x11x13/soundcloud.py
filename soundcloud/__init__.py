82e44174b656a4da32cd340f521c5d32aed7638b
7x11x13 <x7x11x13@gmail.com>
2024-07-08 11:44:46 -0400

    v1.5.1

--- soundcloud/__init__.py
+++ soundcloud/__init__.py
@@ -64,6 +64,6 @@ from soundcloud.resource import __all__ as res_all
 from soundcloud.soundcloud import *
 from soundcloud.soundcloud import __all__ as sc_all
 
-__version__ = "1.5.0"
+__version__ = "1.5.1"
 
 __all__ = sc_all + ex_all + res_all
