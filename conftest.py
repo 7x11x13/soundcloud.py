7789bc5b15ab9bf8f8428a22f6fd3c9b30983a35
Kévin Brèthes <bretheskevin@gmail.com>
2024-08-01 12:51:45 +0200

    fix

--- conftest.py
+++ conftest.py
@@ -7,7 +7,6 @@ from soundcloud import SoundCloud
 
 @pytest.fixture(scope="session")
 def client(request):
-    os.environ["auth_token"] = "2-296070-526801914-fEHP3bawXM2Bew"
     if not os.environ.get("client_id"):
         print("You didn't set the client_id environment variable. One will be generated for you.")
         os.environ["client_id"] = SoundCloud.generate_client_id(SoundCloud)
